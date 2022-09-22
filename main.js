592616ebacba8c41a87b0c4205c21de7b5be03c7
emeralvarado83 <emeralvarado1983@gmail.com>
2022-09-22 19:09:31 -0400

    Se agrego al index el shopping cart, se muestra y oculta al hacer click al carrito de compras, tambien el menu mobile en dispositivos moviles ahora ocupa el 100% del ancho

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const myEmailUser = document.querySelector('.email-user');
 const menuSmallDesktop = document.querySelector('.menu-small-desktop');
 const menuHamIcon = document.querySelector('.menu');
 const menuMobile = document.querySelector('.menu-mobile');
+const cartIcon = document.querySelector('.shopping-cart');
+const cartDetails = document.querySelector('.shopping-cart-details');
 
 myEmailUser.addEventListener('click', toggleMenuUsuario);
 
@@ -13,4 +15,10 @@ menuHamIcon.addEventListener('click', toggleMenuMobile);
 
 function toggleMenuMobile(){
     menuMobile.classList.toggle('inactive');
+}
+
+cartIcon.addEventListener('click', toggleCartDetails);
+
+function toggleCartDetails(){
+    cartDetails.classList.toggle('inactive');
 }
