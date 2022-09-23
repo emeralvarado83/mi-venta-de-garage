e58119fc90104a81caf3bc8146e97f933a03ca4c
emeralvarado83 <emeralvarado1983@gmail.com>
2022-09-22 20:01:08 -0400

    Se agrego en el javascript la siguiente funcion, para dispositivos moviles si abres el menu y luego haces click al carrito de compras se cerrara el menu y viceversa, si esta abierto el carrito de compras al hacer click en el menu se cerrara el carrito, en el css se agrego fondo blanco para el menu, el carrito y a la cuenta de usuario

--- main.js
+++ main.js
@@ -8,17 +8,35 @@ const cartDetails = document.querySelector('.shopping-cart-details');
 myEmailUser.addEventListener('click', toggleMenuUsuario);
 
 function toggleMenuUsuario(){
+    const cartDetailsClosed = cartDetails.classList.contains('inactive');
+
+    if(!cartDetailsClosed){
+        cartDetails.classList.add('inactive');
+    }
+
     menuSmallDesktop.classList.toggle('inactive');
 }
 
 menuHamIcon.addEventListener('click', toggleMenuMobile);
 
 function toggleMenuMobile(){
+    const cartDetailsClosed = cartDetails.classList.contains('inactive');
+
+    if(!cartDetailsClosed){
+        cartDetails.classList.add('inactive');
+    }
+
     menuMobile.classList.toggle('inactive');
 }
 
 cartIcon.addEventListener('click', toggleCartDetails);
 
 function toggleCartDetails(){
+    const menuMobileClosed = menuMobile.classList.contains('inactive');
+
+    if(!menuMobileClosed){
+        menuMobile.classList.add('inactive');
+    }
+
     cartDetails.classList.toggle('inactive');
 }
